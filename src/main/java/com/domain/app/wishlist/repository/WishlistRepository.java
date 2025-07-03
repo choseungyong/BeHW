216836c1fba353a001e0686f2bfa318b4d49584a
choseungyong <csy899700@gmail.com>
2025-07-03 16:39:39 +0900

    수정

--- src/main/java/com/domain/app/wishlist/repository/WishlistRepository.java
+++ src/main/java/com/domain/app/wishlist/repository/WishlistRepository.java
@@ -10,10 +10,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.domain.app.wishlist.domain.Wishlist;
 
-/**
- * WishRepository
- *  - 왜? 반복되는 JPA 쿼리 없이, 메서드명 기반으로 위시 조회·추가·삭제를 편하게 처리하기 위해.
- */
+
 public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
     Page<Wishlist> findAllByMember(Member member, Pageable pageable);
 
